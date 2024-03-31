7824371b733952702cc9508531ba2fce6d866cf0
Ayush <ayushtamrakar.work@gmail.com>
2024-03-31 16:17:02 +0530

    Enabled Eureka server for all microservices

--- Eureka-Server/src/main/java/com/MarketPlace/EurekaServer/EurekaServerApplication.java
+++ Eureka-Server/src/main/java/com/MarketPlace/EurekaServer/EurekaServerApplication.java
@@ -1,17 +1,15 @@
-package com.marketplace.DealerService;
+package com.MarketPlace.EurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 @EnableEurekaServer
-public class DealerServiceApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DealerServiceApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
